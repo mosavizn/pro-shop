0269a77bea64c9de132e1f180b616f7e9be1b093
mousavi@abanppc.com <175136028>
2020-06-24 14:20:51 +0430

    initial commit

---
+++ database/migrations/2020_06_23_203934_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->float('price');
+            $table->float('discount');
+            $table->string('code');
+            $table->boolean('status');
+            $table->boolean('searchable');
+            $table->integer('quantity');
+            $table->string('manufacture_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
